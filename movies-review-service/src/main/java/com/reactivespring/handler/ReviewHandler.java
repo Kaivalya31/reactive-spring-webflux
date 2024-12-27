426bc18bddaec033bf1956b2c4f9b7d7a451c439
Kaivalya31 <kaivalyasourav@gmail.com>
2024-12-27 23:42:11 +0530

    Build MovieReviewService using Functional endpoints.

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,27 +1,115 @@
 package com.reactivespring.handler;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.ReviewDataException;
+import com.reactivespring.exception.ReviewNotFoundException;
 import com.reactivespring.repository.ReviewReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Validator;
+import javax.validation.ConstraintViolation;
+import java.util.stream.Collectors;
+
 @Component//so that it gets scanned as a bean and automatically injected into the bean class
+@Slf4j
 public class ReviewHandler {
+
+    @Autowired
+    private Validator validator;
 
     private ReviewReactiveRepository reviewReactiveRepository;
 
     public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository){
         this.reviewReactiveRepository = reviewReactiveRepository;
     }
+
+    private void validate(Review review){
+        var constraintViolations = validator.validate(review);
+        log.info("constraintsViolations: {}", constraintViolations);
+
+        if (constraintViolations.size() > 0) {
+            var errorMessage = constraintViolations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .sorted()
+                    .collect(Collectors.joining(", "));
+            log.info("errorMessage : {} ", errorMessage);
+            throw new ReviewDataException(errorMessage);
+        }
+    }
+
     public Mono<ServerResponse> addReview(ServerRequest request) {
 
-        return request.bodyToMono(Review.class)
+        return request.bodyToMono(Review.class)    //extract the request to a Mono
+                .doOnNext(this::validate)
                 .flatMap(reviewReactiveRepository::save)//Returns Mono<Review>
                 .flatMap(savedReview -> {
                     return ServerResponse.status(HttpStatus.CREATED).bodyValue(savedReview);
                 });//transform Mono<Review> to Mono<ServerResponse> and return
+    }
+
+    public Mono<ServerResponse> getReviews(ServerRequest request){
+        var movieInfoId = request.queryParam("movieInfoId");
+
+        if(movieInfoId.isPresent()){
+            var reviewsFlux = reviewReactiveRepository.findReviewByMovieInfoId(Long.valueOf(movieInfoId.get()));
+            return ServerResponse.ok().body(reviewsFlux, Review.class);
+        }else{
+            var reviewsFlux = reviewReactiveRepository.findAll();
+            return ServerResponse.ok().body(reviewsFlux, Review.class);
+        }
+    }
+
+    public Mono<ServerResponse> updateReview(ServerRequest request){
+        var reviewId = request.pathVariable("id");
+        var existingReview = reviewReactiveRepository.findById(reviewId)
+                .switchIfEmpty(Mono.error(new ReviewNotFoundException("No review found for Review ID " + reviewId)));
+        //switchIfEmpty() handles the scenario where findById() doesn't return any data
+
+        return existingReview
+                .flatMap(review -> request.bodyToMono(Review.class)
+                .map(reqReview -> {
+                    review.setReviewId(reqReview.getReviewId());
+                    review.setMovieInfoId(reqReview.getMovieInfoId());
+                    review.setComment(reqReview.getComment());
+                    review.setRating(reqReview.getRating());
+
+                    return review;
+                })
+                .flatMap(reviewReactiveRepository::save)
+                .flatMap(savedReview -> ServerResponse.ok().bodyValue(savedReview))
+        );
+
+        /*Alternative approach to handle the scenario where findById() doesn't return any data.
+        We use the switchIfEmpty() while building the response instead of using it after findById().
+        return existingReview
+                .flatMap(review -> request.bodyToMono(Review.class)
+                        .map(reqReview -> {
+                            review.setReviewId(reqReview.getReviewId());
+                            review.setMovieInfoId(reqReview.getMovieInfoId());
+                            review.setComment(reqReview.getComment());
+                            review.setRating(reqReview.getRating());
+
+                            return review;
+                        })
+                        .flatMap(reviewReactiveRepository::save)
+                        .flatMap(savedReview -> ServerResponse.ok().bodyValue(savedReview))
+                )
+                .switchIfEmpty(ServerResponse.notFound().build());  */
+
+    }
+
+    public Mono<ServerResponse> deleteReview(ServerRequest request){
+        var reviewId = request.pathVariable("id");
+        var existingReview = reviewReactiveRepository.findById(reviewId);
+
+        return existingReview
+                .flatMap(review -> reviewReactiveRepository.deleteById(reviewId)
+                        .then(ServerResponse.noContent().build()));
     }
 }
--- movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
+++ movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
@@ -1,4 +1,75 @@
 package com.reactivespring.routes;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.exception.exceptionhandler.GlobalErrorHandler;
+import com.reactivespring.handler.ReviewHandler;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import com.reactivespring.router.ReviewRouter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest
+@ContextConfiguration(classes = {ReviewRouter.class, ReviewHandler.class, GlobalErrorHandler.class})
+@AutoConfigureWebTestClient
 public class ReviewsUnitTest {
+
+    @MockBean
+    private ReviewReactiveRepository reviewReactiveRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private static final String REVIEWS_URL = "/v1";
+
+    @Test
+    void testAddReview(){
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class)))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+
+        webTestClient
+                .post()
+                .uri(REVIEWS_URL + "/addReview")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+                    var savedReview = movieInfoEntityExchangeResult.getResponseBody();
+                    assertNotNull(savedReview);
+                    assertNotNull(savedReview.getReviewId());
+                });
+    }
+
+    @Test
+    void testAddReviewValidation(){
+        var review = new Review(null, null, "Awesome Movie", -9.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class)))
+                .thenReturn(Mono.just(new Review("abc", 1L, "Awesome Movie", 9.0)));
+
+        webTestClient
+                .post()
+                .uri(REVIEWS_URL + "/addReview")
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(String.class)
+                .isEqualTo("review.movieInfoId must not be null, review.rating must not be negative");
+    }
+
+
 }
